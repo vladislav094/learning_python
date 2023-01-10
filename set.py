219179040b697478389c7f9b28cc617bb908203b
Vladislav.H <vkhrabrov94@gmail.com>
2023-01-10 19:32:40 +0300

    added second exercise with set in python

--- set.py
+++ set.py
@@ -16,5 +16,22 @@ arr_of_set = [set(input()) for _ in range(int(input()))]
 print(*general_figures(arr_of_set))
 
 
+def number_first_line(arr_of_str: list) -> list:
+	"""
+	:param arr_of_str:
+	1 2 3 4 5
+	1 2 3 4 6
+	:return:
+	5
+	https://stepik.org/lesson/482377/step/15?unit=473680
+	"""
+	for elt in range(len(arr_of_str)):
+		for _ in range(len(arr_of_str[elt])):
+			arr_of_str[elt][_] = int(arr_of_str[elt][_])
+	myset_1 = set(arr_of_str[0])
+	myset_2 = set(arr_of_str[1])
+	myset_3 = sorted(myset_1.difference(myset_2))
+	return myset_3
 
-
+arr_of_str_with_number = [str(input()).split() for _ in range(2)]
+print(*number_first_line(arr_of_str_with_number))
