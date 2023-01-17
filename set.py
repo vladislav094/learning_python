c0285531359be7f61fce7ad7ab03c14a17ed63ac
Vladislav.H <vkhrabrov94@gmail.com>
2023-01-17 19:56:40 +0300

    added few changes

--- set.py
+++ set.py
@@ -12,8 +12,8 @@ def general_figures(arr_set: list) -> list:
 	base_set = sorted(base_set)
 	return base_set
 
-arr_of_set = [set(input()) for _ in range(int(input()))]
-print(*general_figures(arr_of_set))
+# arr_of_set = [set(input()) for _ in range(int(input()))]
+# print(*general_figures(arr_of_set))
 
 
 def number_first_line(arr_of_str: list) -> list:
@@ -33,5 +33,18 @@ def number_first_line(arr_of_str: list) -> list:
 	myset_3 = sorted(myset_1.difference(myset_2))
 	return myset_3
 
-arr_of_str_with_number = [str(input()).split() for _ in range(2)]
-print(*number_first_line(arr_of_str_with_number))
+# arr_of_str_with_number = [str(input()).split() for _ in range(2)]
+# print(*number_first_line(arr_of_str_with_number))
+
+
+def computer_science_lesson(digit_1: set, digit_2: set, digit_3: set) -> list:
+	"""
+	:param digit_1: 1 5 4 2 5 6 6 2 3 3 5 2
+	:param digit_2: 2 3 5 1 2 1 2 6 7 1 1 6
+	:param digit_3: 1 4 6 9 8 7 0 9 0 9 8 10
+	:return: 5 3 2
+	https://stepik.org/lesson/483114/step/10?unit=474427
+	"""
+	myset_1 = digit_1.intersection(digit_2)
+	myset_2 = sorted(myset_1.difference(digit_3), reverse=True, key=int)
+	return myset_2
