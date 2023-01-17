ce0b9fc7c8efd2ffedd70a7a4026338957f38379
Vladislav.H <vkhrabrov94@gmail.com>
2023-01-17 21:00:43 +0300

    added second function with dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,22 +1,32 @@
-users = [{'name': 'Todd', 'phone': '551-1414', 'email': 'todd@gmail.com'},
-         {'name': 'Helga', 'phone': '555-1618'},
-         {'name': 'Olivia', 'phone': '449-3141', 'email': ''},
-         {'name': 'LJ', 'phone': '555-2718', 'email': 'lj@gmail.net'},
-         {'name': 'Ruslan', 'phone': '422-145-9098', 'email': 'rus-lan.cha@yandex.ru'},
-         {'name': 'John', 'phone': '233-421-32', 'email': ''},
-         {'name': 'Lara', 'phone': '+7998-676-2532', 'email': 'g.lara89@gmail.com'},
-         {'name': 'Alina', 'phone': '+7948-799-2434'},
-         {'name': 'Robert', 'phone': '420-2011', 'email': ''},
-         {'name': 'Riyad', 'phone': '128-8890-128', 'email': 'r.mahrez@mail.net'},
-         {'name': 'Khabib', 'phone': '+7995-600-9080', 'email': 'kh.nurmag@gmail.com'},
-         {'name': 'Olga', 'phone': '6449-314-1213', 'email': ''},
-         {'name': 'Roman', 'phone': '+7459-145-8059'},
-         {'name': 'Maria', 'phone': '12-129-3148', 'email': 'm.sharapova@gmail.com'},
-         {'name': 'Fedor', 'phone': '+7445-341-0545', 'email': ''},
-         {'name': 'Tim', 'phone': '242-449-3141', 'email': 'timm.ggg@yandex.ru'}]
+# users = [{'name': 'Todd', 'phone': '551-1414', 'email': 'todd@gmail.com'},
+#          {'name': 'Helga', 'phone': '555-1618'},
+#          {'name': 'Olivia', 'phone': '449-3141', 'email': ''},
+#          {'name': 'LJ', 'phone': '555-2718', 'email': 'lj@gmail.net'},
+#          {'name': 'Ruslan', 'phone': '422-145-9098', 'email': 'rus-lan.cha@yandex.ru'},
+#          {'name': 'John', 'phone': '233-421-32', 'email': ''},
+#          {'name': 'Lara', 'phone': '+7998-676-2532', 'email': 'g.lara89@gmail.com'},
+#          {'name': 'Alina', 'phone': '+7948-799-2434'},
+#          {'name': 'Robert', 'phone': '420-2011', 'email': ''},
+#          {'name': 'Riyad', 'phone': '128-8890-128', 'email': 'r.mahrez@mail.net'},
+#          {'name': 'Khabib', 'phone': '+7995-600-9080', 'email': 'kh.nurmag@gmail.com'},
+#          {'name': 'Olga', 'phone': '6449-314-1213', 'email': ''},
+#          {'name': 'Roman', 'phone': '+7459-145-8059'},
+#          {'name': 'Maria', 'phone': '12-129-3148', 'email': 'm.sharapova@gmail.com'},
+#          {'name': 'Fedor', 'phone': '+7445-341-0545', 'email': ''},
+#          {'name': 'Tim', 'phone': '242-449-3141', 'email': 'timm.ggg@yandex.ru'}]
+#
+# temp = []
+# for elt in users:
+# 	if 'email' not in elt or elt['email'] == '':
+# 		temp.append(elt['name'])
+# print(*sorted(temp))
 
-temp = []
-for elt in users:
-	if 'email' not in elt or elt['email'] == '':
-		temp.append(elt['name'])
-print(*sorted(temp))
+def string_representation(number: int):
+    dict1 = {0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'}
+    digit = str(number)
+    new_arr = [int(x) for x in digit]
+    for elt in new_arr:
+        if elt in dict1:
+            print(dict1[elt], end=' ')
+digit = int(input())
+string_representation(digit)
