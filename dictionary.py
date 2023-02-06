8039e8ac14d1241ce278d2b4d21855e6293c1a5d
Vladislav.H <vkhrabrov94@gmail.com>
2023-02-06 20:29:36 +0300

    added several dictionary generator. I continue to study dictionary in Python3 on Stepik

--- dictionary.py
+++ dictionary.py
@@ -408,7 +408,22 @@
 #
 # print(secret_word(arr_input,my_dict))
 
-s = '1:men 2:kind 90:number 0:sun 34:book 56:mountain 87:wood 54:car 3:island 88:power 7:box 17:star 101:ice'
-arr = s.split()
-result = {int(k): v for k, v in [a.split(':') for a in s.split()]}
+# numbers = [34, 10, -4, 6, 10, 23, -90, 100, 21, -35, -95, 1, 36, -38, -19, 1, 6, 87]
+# result = {k: numbers[k]**2 for k in range(len(numbers))}
+#
+# colors = {'c1': 'Red', 'c2': 'Grey', 'c3': None, 'c4': 'Green', 'c5': 'Yellow', 'c6': 'Pink', 'c7': 'Orange', 'c8': None, 'c9': 'White', 'c10': 'Black', 'c11': 'Violet', 'c12': 'Gold', 'c13': None, 'c14': 'Amber', 'c15': 'Azure', 'c16': 'Beige', 'c17': 'Bronze', 'c18': None, 'c19': 'Lilac', 'c20': 'Pearl', 'c21': None, 'c22': 'Sand', 'c23': None}
+# result = {k: colors[k] for k in colors if colors[k] != None}
+#
+# favorite_numbers = {'timur': 17, 'ruslan': 7, 'larisa': 19, 'roman': 123, 'rebecca': 293, 'ronald': 76, 'dorothy': 62, 'harold': 36, 'matt': 314, 'kim': 451, 'rosaly': 18, 'rustam': 89, 'soltan': 111, 'amir': 654, 'dima': 390, 'amiran': 777, 'geor': 999, 'sveta': 75, 'rita': 909, 'kirill': 404, 'olga': 271, 'anna': 55, 'madlen': 876}
+# result = {k: favorite_numbers[k] for k in favorite_numbers if len(str(favorite_numbers[k])) == 2}
+#
+#
+# months = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'}
+# result = {v: k for k, v in months.items()}
+#
+# s = '1:men 2:kind 90:number 0:sun 34:book 56:mountain 87:wood 54:car 3:island 88:power 7:box 17:star 101:ice'
+# result = {int(k): v for k, v in [a.split(':') for a in s.split()]}
+
+numbers = [34, 10, 4, 6, 10, 23, 90, 100, 21, 35, 95, 1, 36, 38, 19, 1, 6, 87, 1000, 13456, 360]
+result = {numbers[k]: sorted([v for v in range(1, numbers[k]+1) if numbers[k] % v == 0]) for k in range(len(numbers))}
 print(result)
