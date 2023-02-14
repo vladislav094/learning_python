240413726d2b4c56e8d5f712fe3d3c213e3ed2ee
Vladislav.H <vkhrabrov94@gmail.com>
2023-02-14 23:04:41 +0300

    I started solving problems on codewars

--- random_file.py
+++ random_file.py
@@ -179,15 +179,36 @@ from math import factorial
 # print(f"{first} / {second} = {Fraction(first) / Fraction(second)}")
 
 
-from math import gcd
-from fractions import Fraction
-n = int(input())
-result = []
-while n != 1:
-    for i in range(1, n):
-        if gcd(i, n) == 1:
-            result.append(f'{i}/{n}')
-    n -= 1
-answer = sorted(map(Fraction, result))
-print(*answer, sep='\n')
+# from math import gcd
+# from fractions import Fraction
+# n = int(input())
+# result = []
+# while n != 1:
+#     for i in range(1, n):
+#         if gcd(i, n) == 1:
+#             result.append(f'{i}/{n}')
+#     n -= 1
+# answer = sorted(map(Fraction, result))
+# print(*answer, sep='\n')
 
+
+a1 = ["arp", "live", "strong"]
+
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+# a1 = ["tarp", "mice", "bull"]
+
+# a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+
+def in_array(array1, array2):
+    temp = []
+    count = 0
+    for elt in range(len(array1)):
+        for jet in range(len(array2)):
+            if array1[elt] in array2[jet] and array1[elt] not in temp:
+                count += 1
+                temp.append(array1[elt])
+    temp.sort()
+    return temp
+
+print(in_array(a1, a2))
