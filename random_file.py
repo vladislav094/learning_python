2927e3de86a72ad6008b012d6b51f2fc95229b04
Vladislav.H <vkhrabrov94@gmail.com>
2023-02-25 20:21:19 +0300

    added code with function how key for sorted

--- random_file.py
+++ random_file.py
@@ -402,12 +402,19 @@ def mean(*args):
 # print(pairs)
 
 
-numbers = [(10, 10, 10), (30, 45, 56), (81, 39), (1, 2, 3), (12,), (-2, -4, 100), (1, 2, 99), (89, 9, 34),
-           (10, 20, 30, -2), (50, 40, 50), (34, 78, 65), (-5, 90, -1, -5), (1, 2, 3, 4, 5, 6), (-9, 8, 4), (90, 1, -45, -21)]
+# numbers = [(10, 10, 10), (30, 45, 56), (81, 39), (1, 2, 3), (12,), (-2, -4, 100), (1, 2, 99), (89, 9, 34),
+#            (10, 20, 30, -2), (50, 40, 50), (34, 78, 65), (-5, 90, -1, -5), (1, 2, 3, 4, 5, 6), (-9, 8, 4), (90, 1, -45, -21)]
+#
+# def comparator(x):
+#     return sum(x) / len(x)
+#
+# print(min(numbers, key=comparator))
+# print(max(numbers, key=comparator))
+
+points = [(-1, 1), (5, 6), (12, 0), (4, 3), (0, 1), (-3, 2), (0, 0), (-1, 3), (2, 0), (3, 0), (-9, 1), (3, 6), (8, 8)]
 
 def comparator(x):
-    return sum(x) / len(x)
+    return ((x[0] ** 2) + (x[1] ** 2)) ** 0.5
 
-print(min(numbers, key=comparator))
-print(max(numbers, key=comparator))
+print(sorted(points, key=comparator))
 
