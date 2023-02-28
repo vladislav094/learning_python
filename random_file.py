351a803d1ef5cecf691699dc2acd375af71a9a42
Vladislav.H <vkhrabrov94@gmail.com>
2023-02-28 21:08:55 +0300

    added map function with lambda in body for exercise "Opposite color"

--- random_file.py
+++ random_file.py
@@ -883,16 +883,22 @@ def mean(*args):
 
 
 
-mixed_list = ['beside', 48, 'accelerate', 28, 'beware', 'absorb', 'besides', 'berry', 15, 65, 'abate', 'thursday',
-              76, 70, 94, 35, 36, 'berth', 41, 'abnormal', 'bicycle', 'bid', 'sunday', 'saturday', 87, 'bigot', 41,
-              'abort', 13, 60, 'friday', 26, 13, 'accident', 'access', 40, 26, 20, 75, 13, 40, 67, 12, 'abuse', 78,
-              10, 80, 'accessory', 20, 'bewilder', 'benevolent', 'bet', 64, 38, 65, 51, 95, 'abduct', 37, 98, 99, 14,
-              'abandon', 'accept', 46, 'abide', 'beyond', 19, 'about', 76, 26, 'abound', 12, 95, 'wednesday', 'abundant',
-              'abrupt', 'aboard', 50, 89, 'tuesday', 66, 'bestow', 'absent', 76, 46, 'betray', 47, 'able', 11]
+# mixed_list = ['beside', 48, 'accelerate', 28, 'beware', 'absorb', 'besides', 'berry', 15, 65, 'abate', 'thursday',
+#               76, 70, 94, 35, 36, 'berth', 41, 'abnormal', 'bicycle', 'bid', 'sunday', 'saturday', 87, 'bigot', 41,
+#               'abort', 13, 60, 'friday', 26, 13, 'accident', 'access', 40, 26, 20, 75, 13, 40, 67, 12, 'abuse', 78,
+#               10, 80, 'accessory', 20, 'bewilder', 'benevolent', 'bet', 64, 38, 65, 51, 95, 'abduct', 37, 98, 99, 14,
+#               'abandon', 'accept', 46, 'abide', 'beyond', 19, 'about', 76, 26, 'abound', 12, 95, 'wednesday', 'abundant',
+#               'abrupt', 'aboard', 50, 89, 'tuesday', 66, 'bestow', 'absent', 76, 46, 'betray', 47, 'able', 11]
+#
+# # new_list = sorted(mixed_list, key= lambda x: (isinstance(x, str), x))
+# new_list = sorted(list(filter(lambda x: (isinstance(x, int)), mixed_list)))
+# new_list_str = sorted(list(filter(lambda x: (isinstance(x, str)), mixed_list)))
+# print(new_list)
+# print(new_list_str)
+# print(*new_list + new_list_str)
 
-# new_list = sorted(mixed_list, key= lambda x: (isinstance(x, str), x))
-new_list = sorted(list(filter(lambda x: (isinstance(x, int)), mixed_list)))
-new_list_str = sorted(list(filter(lambda x: (isinstance(x, str)), mixed_list)))
-print(new_list)
-print(new_list_str)
-print(*new_list + new_list_str)
+# three_colors = str(input()).split()
+# print(three_colors)
+color = ['244', '11', '120']
+new_colors = list(map(lambda x: 255 - int(x), color))
+print(new_colors)
