bbd3710220168930f0fa709790e3e7b8c352e790
Vladislav.H <vkhrabrov94@gmail.com>
2023-02-28 19:56:25 +0300

    I continue study labda function in Python3 on Stepik. Day 2.

--- random_file.py
+++ random_file.py
@@ -839,12 +839,20 @@ def mean(*args):
 # print(*new_words)
 
 
-numbers = [46, 61, 34, 17, 56, 26, 93, 1, 3, 82, 71, 37, 80, 27, 77, 94, 34, 100, 36, 81, 33, 81, 66, 83, 41, 80, 80,
-           93, 40, 34, 32, 16, 5, 16, 40, 93, 36, 65, 8, 19, 8, 75, 66, 21, 72, 32, 41, 59, 35, 64, 49, 78, 83, 27,
-           57, 53, 43, 35, 48, 17, 19, 40, 90, 57, 77, 56, 80, 95, 90, 27, 26, 6, 4, 23, 52, 39, 63, 74, 15, 66, 29,
-           88, 94, 37, 44, 2, 38, 36, 32, 49, 5, 33, 60, 94, 89, 8, 36, 94, 46, 33]
+# numbers = [46, 61, 34, 17, 56, 26, 93, 1, 3, 82, 71, 37, 80, 27, 77, 94, 34, 100, 36, 81, 33, 81, 66, 83, 41, 80, 80,
+#            93, 40, 34, 32, 16, 5, 16, 40, 93, 36, 65, 8, 19, 8, 75, 66, 21, 72, 32, 41, 59, 35, 64, 49, 78, 83, 27,
+#            57, 53, 43, 35, 48, 17, 19, 40, 90, 57, 77, 56, 80, 95, 90, 27, 26, 6, 4, 23, 52, 39, 63, 74, 15, 66, 29,
+#            88, 94, 37, 44, 2, 38, 36, 32, 49, 5, 33, 60, 94, 89, 8, 36, 94, 46, 33]
+#
+# new_numbers = list(filter(lambda x: x if x < 47 or x > 47 and x % 2 ==0 else None, numbers))
+# new_numbers = list(map(lambda x: x // 2 if x % 2 == 0 else x, new_numbers))
+#
+# print(*new_numbers)
 
-new_numbers = list(filter(lambda x: x if x < 47 or x > 47 and x % 2 ==0 else None, numbers))
-new_numbers = list(map(lambda x: x // 2 if x % 2 == 0 else x, new_numbers))
+data = [(19542209, 'New York'), (4887871, 'Alabama'), (1420491, 'Hawaii'), (626299, 'Vermont'), (1805832, 'West Virginia'),
+        (39865590, 'California'), (11799448, 'Ohio'), (10711908, 'Georgia'), (10077331, 'Michigan'), (10439388, 'Virginia'),
+        (7705281, 'Washington'), (7151502, 'Arizona'), (7029917, 'Massachusetts'), (6910840, 'Tennessee')]
 
-print(*new_numbers)
+
+data = list(sorted(data, key=lambda x: x[1][-1], reverse=True))
+print(*list(map(lambda x: f"{x[1]}: {x[0]}", data)),sep='\n')
