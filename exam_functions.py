0c76a8039ddc42d85870947f5984bce41d42578e
Vladislav.H <vkhrabrov94@gmail.com>
2023-03-04 11:35:36 +0300

    added second solution with exam task

---
+++ exam_functions.py
@@ -0,0 +1,27 @@
+#1
+# def generate_letter(mail, name, date, time, place, teacher='Тимур Гуев', number=17):
+#     return f"""To: {mail}
+# Приветствую, {name}!
+# Вам назначен экзамен, который пройдет {date}, в {time}.
+# По адресу: {place}.
+# Экзамен будет проводить {teacher} в кабинете {number}.
+# Желаем удачи на экзамене!"""
+# print(generate_letter('lara@yandex.ru', 'Лариса', '10 декабря', '12:00', 'Часова 23, корпус 2'))
+# print()
+# print(generate_letter('lara@yandex.ru', 'Лариса', '10 декабря', '12:00',
+#                       'Часова 23, корпус 2', 'Василь Ярошевич', 23))
+
+
+#2
+def pretty_print(*data, side='-', delimiter='|'):
+	s2 = delimiter + " " + f' {delimiter} '.join(map(str, *data)) + " " + delimiter
+	s = " " + (len(s2) - 2) * side + " "
+	print(s)
+	print(s2)
+	print(s)
+
+pretty_print([1, 2, 10, 23, 123, 3000])
+pretty_print(['abc', 'def', 'ghi', '12345'])
+pretty_print(['abc', 'def', 'ghi'], side='*')
+pretty_print(['abc', 'def', 'ghi'], delimiter='#')
+pretty_print(['abc', 'def', 'ghi'], side='*', delimiter='#')
