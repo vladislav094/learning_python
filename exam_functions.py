e5a2cec604f8fc372c7834e5219db14542e4f245
Vladislav.H <vkhrabrov94@gmail.com>
2023-03-04 13:58:56 +0300

    added solution #6

--- exam_functions.py
+++ exam_functions.py
@@ -48,5 +48,9 @@ from functools import reduce
 # print(product_of_odds(arr))
 
 #5
-words = 'the world is mine take a look what you have started'.split()
-print(*list(map(lambda x: '"'+ x + '"', words)))
+# words = 'the world is mine take a look what you have started'.split()
+# print(*list(map(lambda x: '"'+ x + '"', words)))
+
+#6
+numbers = [18, 191, 9009, 5665, 78, 77, 45, 23, 19991, 908, 8976, 6565, 5665, 10, 1000, 908, 909, 232, 45654, 786]
+print(*list(filter(lambda x: str(x) != str(x)[::-1], numbers)))
