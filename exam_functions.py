4599fb8b39e1aa5b01fdfae5be00c2a1720ddccb
Vladislav.H <vkhrabrov94@gmail.com>
2023-03-04 14:02:38 +0300

    added solution #7

--- exam_functions.py
+++ exam_functions.py
@@ -52,5 +52,12 @@ from functools import reduce
 # print(*list(map(lambda x: '"'+ x + '"', words)))
 
 #6
-numbers = [18, 191, 9009, 5665, 78, 77, 45, 23, 19991, 908, 8976, 6565, 5665, 10, 1000, 908, 909, 232, 45654, 786]
-print(*list(filter(lambda x: str(x) != str(x)[::-1], numbers)))
+# numbers = [18, 191, 9009, 5665, 78, 77, 45, 23, 19991, 908, 8976, 6565, 5665, 10, 1000, 908, 909, 232, 45654, 786]
+# print(*list(filter(lambda x: str(x) != str(x)[::-1], numbers)))
+
+
+#7
+# numbers = [(10, -2, 3, 4), (-13, 56), (1, 9, 2), (-1, -9, -45, 32), (-1, 5, 1), (17, 0, 1), (0, 1), (3,), (39, 12), (11, -23), (10, -100, 21, 32), (3, -8), (1, 1)]
+# sorted_numbers = sorted(numbers, key=lambda x:sum(x) / len(x), reverse=True)
+# print(sorted_numbers)
+
