d7a4e819556b4ab87d898cc0bcd6344c24561d98
Vladislav.H <vkhrabrov94@gmail.com>
2023-03-05 14:06:44 +0300

    start study unit 17 Files

--- random_file.py
+++ random_file.py
@@ -988,14 +988,25 @@ def mean(*args):
 #
 # print('YES' if all(progress) else 'NO')
 
-def generate_letter(mail, name, date, time, place, teacher='Тимур Гуев', number=17):
-    return f"""To: {mail}           
-Приветствую, {name}! 
-Вам назначен экзамен, который пройдет {date}, в {time}.
-По адресу: {place}.
-Экзамен будет проводить {teacher} в кабинете {number}.
-Желаем удачи на экзамене!"""
-print(generate_letter('lara@yandex.ru', 'Лариса', '10 декабря', '12:00', 'Часова 23, корпус 2'))
-print()
-print(generate_letter('lara@yandex.ru', 'Лариса', '10 декабря', '12:00',
-                      'Часова 23, корпус 2', 'Василь Ярошевич', 23))
+# def generate_letter(mail, name, date, time, place, teacher='Тимур Гуев', number=17):
+#     return f"""To: {mail}
+# Приветствую, {name}!
+# Вам назначен экзамен, который пройдет {date}, в {time}.
+# По адресу: {place}.
+# Экзамен будет проводить {teacher} в кабинете {number}.
+# Желаем удачи на экзамене!"""
+# print(generate_letter('lara@yandex.ru', 'Лариса', '10 декабря', '12:00', 'Часова 23, корпус 2'))
+# print()
+# print(generate_letter('lara@yandex.ru', 'Лариса', '10 декабря', '12:00',
+#                       'Часова 23, корпус 2', 'Василь Ярошевич', 23))
+
+
+file = open('prices.txt')
+summa = 0
+
+for line in file.readlines():
+    arr = line.split()
+    summa += int(arr[1]) * int(arr[2])
+
+print(summa)
+file.close()
