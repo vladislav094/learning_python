d6e3c4f0aad75846aa3f2b0dd31546753aa20a75
Vladislav.H <vkhrabrov94@gmail.com>
2023-03-11 14:16:24 +0300

    added solution for task #4 in exam on files

--- files_exam.py
+++ files_exam.py
@@ -11,12 +11,21 @@
 # 	print(f"${sum(arr)}")
 
 #3
+# with open('students.txt', 'r', encoding='utf-8') as file:
+# 	arr = file.readlines()
+# 	count = 0
+# 	for elt in arr:
+# 		person = elt.split()[1:]
+# 		person = [int(x) for x in person]
+# 		if all([x >= 65 for x in person]):
+# 			count +=1
+# 	print(count)
+
+
+#4
 with open('students.txt', 'r', encoding='utf-8') as file:
-	arr = file.readlines()
-	count = 0
-	for elt in arr:
-		person = elt.split()[1:]
-		person = [int(x) for x in person]
-		if all([x >= 65 for x in person]):
-			count +=1
-	print(count)
+	arr = file.read()
+	len_max = max([len(x) for x in arr.split()])
+	for elt in arr.split():
+		if len(elt) == len_max:
+			print(elt)
