8957b2c33f0575e56c4cdaf7070c9d47d780aa39
Vladislav.H <vkhrabrov94@gmail.com>
2023-03-11 14:02:33 +0300

    added solition for task #2 in exam on files

--- files_exam.py
+++ files_exam.py
@@ -1,4 +1,11 @@
 #1
-with open(input(), 'r', encoding='utf-8') as file:
-    arr = file.readlines()
-    print(len(arr))
+# with open(input(), 'r', encoding='utf-8') as file:
+#     arr = file.readlines()
+#     print(len(arr))
+
+
+#2
+with open('students.txt', 'r', encoding='utf-8') as file:
+	arr = file.readlines()
+	arr = [int(x.replace('$', '')) for x in arr]
+	print(f"${sum(arr)}")
