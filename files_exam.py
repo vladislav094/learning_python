ec9f5ac609bbfbc1360c726c77c2e633d86ede93
Vladislav.H <vkhrabrov94@gmail.com>
2023-03-11 14:10:05 +0300

    added solition for task #3 in exam on files

--- files_exam.py
+++ files_exam.py
@@ -5,7 +5,18 @@
 
 
 #2
+# with open('students.txt', 'r', encoding='utf-8') as file:
+# 	arr = file.readlines()
+# 	arr = [int(x.replace('$', '')) for x in arr]
+# 	print(f"${sum(arr)}")
+
+#3
 with open('students.txt', 'r', encoding='utf-8') as file:
 	arr = file.readlines()
-	arr = [int(x.replace('$', '')) for x in arr]
-	print(f"${sum(arr)}")
+	count = 0
+	for elt in arr:
+		person = elt.split()[1:]
+		person = [int(x) for x in person]
+		if all([x >= 65 for x in person]):
+			count +=1
+	print(count)
