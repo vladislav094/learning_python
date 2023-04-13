51765b2c5d2def42246b8c98e5491685bf2fc9e0
Vladislav.H <vkhrabrov94@gmail.com>
2023-04-13 21:58:57 +0300

    continue study oop

--- oop/codewars.py
+++ oop/codewars.py
@@ -1,29 +1,47 @@
-class Block:
-    # Good Luck!
-    def __init__(self, arr):
-        self.arr = list(arr)
+# 1
 
-    def get_width(self):
-        return self.arr[0]
+# class Block:
+#     # Good Luck!
+#     def __init__(self, arr):
+#         self.arr = list(arr)
+#
+#     def get_width(self):
+#         return self.arr[0]
+#
+#     def get_length(self):
+#         return self.arr[1]
+#
+#     def get_height(self):
+#         return self.arr[2]
+#
+#     def get_volume(self):
+#         return self.arr[0] * self.arr[1] * self.arr[2]
+#
+#     def get_surface_area(self):
+#         return 2 * (self.arr[1] * self.arr[0] + self.arr[1] * self.arr[2] + self.arr[0] * self.arr[2])
+#
+#
+# b = Block([2, 4, 6])
+# print(b)
+#
+# print(b.get_width())
+# print(b.get_length())
+# print(b.get_height())
+# print(b.get_volume())
+# print(b.get_surface_area())
 
-    def get_length(self):
-        return self.arr[1]
+# 2
 
-    def get_height(self):
-        return self.arr[2]
-
-    def get_volume(self):
-        return self.arr[0] * self.arr[1] * self.arr[2]
-
-    def get_surface_area(self):
-        return 2 * (self.arr[1] * self.arr[0] + self.arr[1] * self.arr[2] + self.arr[0] * self.arr[2])
+class Class:
+    value = 1
+    @staticmethod
+    def get_number():
+        result = Class.value
+        Class.value *= 2
+        return result
 
 
-b = Block([2, 4, 6])
-print(b)
-
-print(b.get_width())
-print(b.get_length())
-print(b.get_height())
-print(b.get_volume())
-print(b.get_surface_area())
+print(Class.get_number())
+print(Class.get_number())
+print(Class.get_number())
+print(Class.get_number())
--- oop/listinstance.py
+++ oop/listinstance.py
@@ -1,21 +1,42 @@
-class ListInstance:
+# class ListInstance:
+#
+#     def __attrnames(self):
+#         result = ''
+#         for attr in sorted(self.__dict__):
+#             result += f'\t{attr} = {self.__dict__[attr]} \n'
+#         return result
+#
+#     def __str__(self):
+#         return f"Instance of {self.__class__.__name__}," \
+#                f"{id(self)}," \
+#                f"{self.__attrnames()}"
+#
+#
+# class Spam(ListInstance):
+#     def __init__(self):
+#         self.data1 = 'food'
+#
+#
+# x = Spam()
+# print(x)
 
-    def __attrnames(self):
-        result = ''
-        for attr in sorted(self.__dict__):
-            result += f'\t{attr} = {self.__dict__[attr]} \n'
-        return result
 
-    def __str__(self):
-        return f"Instance of {self.__class__.__name__}," \
-               f"{id(self)}," \
-               f"{self.__attrnames()}"
+class People:
+    def __init__(self, name, old):
+        self.__name = name
+        self.__old = old
 
+    @property
+    def get_old(self):
+        return self.__old
 
-class Spam(ListInstance):
-    def __init__(self):
-        self.data1 = 'food'
+    @get_old.setter
+    def get_old(self, old):
+        self.__old = old
 
+    # old = property(get_old, set_old)
 
-x = Spam()
-print(x)
+a = People('Ivan', 25)
+print(a.get_old)
+a.get_old = 30
+print(a.get_old)
