1138d34e2e5fcc4363839d946ee09bd292f7f121
Vladislav.H <vkhrabrov94@gmail.com>
2023-04-15 13:09:25 +0300

    added another one solution

--- oop/codewars.py
+++ oop/codewars.py
@@ -95,6 +95,40 @@
 
 
 # 6
+class Student:
+
+    def __init__(self, first_name, last_name, grades=[]):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.grades = grades[:]
+
+    def add_grade(self, grade):
+        self.grades.append(grade)
+
+    def get_average(self):
+        return sum(self.grades) / len(self.grades)
+
+matthewConnorGrades = [44, 53, 27, 60]
+chloeMadisonGrades = [79, 58, 30, 66]
+studentGrades = matthewConnorGrades, chloeMadisonGrades
+matthewConnor = Student('Matthew', 'Connor', matthewConnorGrades)
+chloeMadison = Student('Chloe', 'Madison', chloeMadisonGrades)
+students = matthewConnor, chloeMadison
+print(matthewConnor.grades)
+print(chloeMadison.grades)
 
 
+johnDoe = Student('John', 'Doe')
+janeDoe = Student('Jane', 'Doe')
+jamesSmith = Student('James', 'Smith')
+jennaSmith = Student('Jenna', 'Smith')
+students = johnDoe, janeDoe, jamesSmith, jennaSmith
+firstAssessmentGrades = [63, 92, 82, 75]
+#
+for i, student in enumerate(students):
+    student.add_grade(firstAssessmentGrades[i])
 
+print(johnDoe.grades)
+print(janeDoe.grades)
+print(jamesSmith.grades)
+print(jennaSmith.grades)
