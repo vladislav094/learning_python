6870468fca8e2155ebf9899bb5b9833f00461fcc
Vladislav.H <vkhrabrov94@gmail.com>
2023-04-24 22:19:55 +0300

    added next solution

--- professional/part-1.py
+++ professional/part-1.py
@@ -1,9 +1,25 @@
 #1
-card = '3456 9012 5678 1234'
-def hide_card(card_number: str)-> str:
-	card_number = card_number.replace(' ', '')
-	card_number = '*' * 12 + card_number[12:]
-	return card_number
-print(hide_card(card))
+# card = '3456 9012 5678 1234'
+# def hide_card(card_number: str)-> str:
+# 	card_number = card_number.replace(' ', '')
+# 	card_number = '*' * 12 + card_number[12:]
+# 	return card_number
+# print(hide_card(card))
+
+#2
+
+arr = [6, 0, 67, -7, 10, -20]
+def same_parity(numbers):
+	result = []
+	if len(numbers) == 0:
+		return []
+	else:
+		x = numbers[0]
+		for elt in numbers:
+			if elt % 2 == x % 2:
+				result.append(elt)
+	return result
+
+print(same_parity(arr))
 
 
